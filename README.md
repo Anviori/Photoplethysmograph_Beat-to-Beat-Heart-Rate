1f195186d2e2f3dff861f62cb11f07a87e98c414
Wilmer Suarez <Wilmer.suarez1230@gmail.com>
2019-09-09 15:58:46 -0400

    More code cleanup

--- README.md
+++ README.md
@@ -6,51 +6,52 @@
 - [Schematics](#schematics)
 - [Demo Video](#demo-video)
 
-## Introduction 
+## Introduction
 
-AVR Assembler was used to program an ATmega16A to build an embedded system 
+AVR Assembler was used to program an ATmega16A to build an embedded system
 (A plethysmograph) that monitors heart rate with auto-calibration.
 
-*A plethysmograph is an instrument for measuring changes in volume within an 
-organ of the body.*
+*A plethysmograph is an instrument for measuring changes in volume within an organ of the body.*
 
-The system uses optics to obtain a photoplethysmogram (PPG) (The a signal 
-obtained by the plethysmograph measurement).
+The system uses optics to obtain a photoplethysmogram (PPG)
+(The a signal obtained by the plethysmograph measurement).
 
-To aquire this signal, two Infrared LEDs are used to transmit light 
-through the finger tip and an Infrared Photo-transistor detects the light on 
-the other side. The pressure pulse of the cardiac cycle increases the blood 
-volume in the finger tip and more light is absorbed. This light causes its collector 
-voltage of the photo-transistor to increase. This voltage value is the 
-photoplethysmogram signal used to derive the heart rate. The DC component of the signal 
-obtained is attributable to the absorption of the skin tissue. The AC component is 
-attributable to the variation in blood volume in the skin caused by the pressure pulse of 
-the cardiac cycle.
+To aquire this signal, two Infrared LEDs are used to transmit light
+through the finger tip and an Infrared Photo-transistor detects the light on the
+other side. The pressure pulse of the cardiac cycle increases the blood volume in
+the finger tip and more light is absorbed. This light causes the collector
+voltage of the photo-transistor to increase. This voltage value is the
+photoplethysmogram signal used to derive the heart rate.
+The DC component of the signal obtained is attributable to the absorption of the
+skin tissue. The AC component is attributable to the variation in blood volume in
+the skin caused by the pressure pulse of the cardiac cycle.
 
-The two most common measurements derived from a photoplethysmogram signal are heart rate 
-and blood oxygen saturation (SpO2). This system includes an analog front end circuit that
-models a photoplethysmo-graph and, using the MCU's input capture feature, measures the 
-heart rate. [*Schematic: Figure1*](#figure1-analog-front-end-circuit)
+The two most common measurements derived from a photoplethysmogram signal are
+heart rate and blood oxygen saturation (SpO2). This system includes an analog
+front end circuit that models a plethysmograph and, using the MCU's input
+capture feature, measures the period of the signal to calculate the heart rate. [*Schematic: Figure1*](#figure1-analog-front-end-circuit)
 
-The infrared LED's used a 100 ohm potentiometer to set the current. This needed to be replaced
-to allow automatic adjustments in current through the LEDs for different individuals. First,
-an ideal value for the photo-transistor must be found when someones finger tip is between the
-infrared diodes and the photo-transistor. A voltage divider connected to the supply voltage is
+The infrared emitting LED's used a 100 ohm potentiometer to set the current. This
+was replaced to allow automatic adjustments in current through the LEDs for
+different individuals. First, an ideal value for the photo-transistor must be
+found when someones finger tip is between the infrared diodes and the
+photo-transistor. A voltage divider connected to the supply voltage is
 adjusted to aquire the ideal voltage. This voltage is the set-point voltage. [*Schematic: Figure2*](#figure2-autocalibration)
 
-A digital potentiometer is used to automatically adjust the current through the infrared LEDs 
-until the photo-transistor's collector voltage is equal to the set-point voltage 
-(this was done using the MCU's analog comparator); a voltage between 0.3V and 1.0V. [*Schematic: Figure3*](#figure3-analog-front-end-circuit-with-digital-potentiometer)
+The digital potentiometer is used to automatically adjust the current through the
+infrared LEDs until the photo-transistor's collector voltage is equal to the
+set-point voltage (this was done using the MCU's analog comparator); a voltage
+between 0.3V and 1.0V. [*Schematic: Figure3*](#figure3-analog-front-end-circuit-with-digital-potentiometer)
 
-The system measures heart rates from 40 to 200 bpm and displays them on an LCD with a resolution 
-of 0.1 bpm. [*System Demo Video*](#demo-video)
+The system measures heart rates from 40 to 200 bpm and displays them on an LCD
+with a resolution of 0.1 bpm. [*System Demo Video*](#demo-video)
 
 ## Schematics
 
 ###### Figure1 Analog Front End Circuit
 ![alt text](http://i.imgur.com/9PJWSe1.png "Analog front end circuit")
 
-###### Figure2 Autocalibration 
+###### Figure2 Autocalibration
 ![alt text](http://i.imgur.com/gjlVa3j.png "Analog front end circuit")
 
 ###### Figure3 Analog Front End Circuit With Digital Potentiometer
--- main.asm
+++ main.asm
@@ -2,113 +2,121 @@
 ; Project Title: Beat to Beat Heart Rate Monitor with Autocalibration
 ; Author: Wilmer Suarez
 ; Version: 3.0
-; Last updated: --/--/19
+; Last updated: 09/07/19
 ; Target: ATmega16 @ 1MHz
 ; Total number of words:
 ; Total number of cycles:
 ;
-; DESCRIPTION: Using Timer/Counter1, this program measures the period of the
-; signal coming from the analog front end circuit. This is done using Input
-; Capture. The period is measured and, from it, the frequency is calculated
-; and displayed on the LCD Module. This calculated frequency ~= heart reate.
+; DESCRIPTION: Using Timer/Counter1's Input Capture, this program measures the
+; period of the signal coming from the analog front end circuit. The period is
+; measured and, from it, the frequency is calculated and displayed on the LCD
+; Module. The calculated frequency == heart reate.
 ;
 ; VERSION HISTORY
 ; 1.0 Base Version
 ; 2.0 Modified for Auto-Calibration
 ; 3.0 Optimized Code / Cleanup
 ;==============================================================================
-
-; Do not show device denition include file(s) in output listfile
+; Do not show device register/bit definitions include file in output listfile
 .nolist
 .include "m16def.inc"
 .list
-; LCD DOG driver
-.include "lcd_dogm08_asm_driver_m16_pin_assign.inc"
-; 32-bit division subroutine
-.include "div32u.inc"
+.include "lcd_dogm08_asm_driver_m16_pin_assign.inc" ; LCD DOG driver
+.include "div32u.inc" ; 32-bit division subroutine
+.include "bin2BCD16.inc" ; C
 
-*** TODO: FIX ALL TITLES v ***
-//---------------------------Variables (what type of variables (where?)---------------------------//
-.dseg
-counting:	   .byte 1 ; flag = 1 when counter is counting
-meas_complete: .byte 1 ; flag = 1 when measurement is complete
-count_low:	   .byte 1 ; low byte of count read from counter1
-count_high:	   .byte 1 ; high byte of count read from counter1
+.dseg ; In SRAM
+counting:	   .byte 1 ; flag == 1 when counter is counting
+meas_complete: .byte 1 ; flag == 1 when measurement is complete
+count_low:	   .byte 1 ; low byte of count read from counter1 (16-bit counter)
+count_high:	   .byte 1 ; high byte of count read from counter1 (16-bit counter)
 
-//---------------Vector Table---------------//
-.cseg
+;===============INTERRUPT VECTOR TABLE===============;
+.cseg ; In Program Memory
+
+; This constant is used in the calculation to convert the measured period of the PPG signal
+; to cycles per minute (cpm)(the heart rate), with a precision of 0.1.
+; Constant derivation:
+;	F(0.1cpm) = 60/(period_count)*256us --> F = 600/(period_count)*256us -->
+;	F = 600000000/(period_count)*256 --> F = 2343750/(period_count)
+.equ CPM_CONSTANT 0x23C346
+
 reset:
-.org RESET		   ; resest interrupt vector (Program starts here at reset)
+.org RESET		   ; "Resest" interrupt vector (Program starts here at reset)
 	rjmp start
-.org ICP1addr      ; ICP1 interrupt vector
+.org ICP1addr      ; Timer/Counter1 Input Capture interrupt vector
 	rjmp count_ISR
 
 start:
-//-------------Port Configuration-------------//
-	sbi DDRA, 0
+	;===============PORT CONFIGURATION===============;
+	sbi DDRA, 0 ; Port A, Pin0 = LCD module, register select pin
 
 	sbi DDRC, 0 ; Port C, Pin0 as output for chip select
-
 	sbi DDRC, 1 ; Port C, Pin1 as output to LED
 
-	ldi r16, $3F  ; PORT D, Pin6 as input of Vout for analog front end circuit
-	out DDRD, r16 ; Pin7 = input for pushbutton
-
-	ldi r16, $80   ; Enabling pull-up resistor
+	ldi r16, $3F   ; PORT D, Pin6 as input for "Vout" signal from analog front end circuit
+				   ; (photoplethysmogram - PPG)
+	out DDRD, r16  ; Pin7 = input for Calibration-start pushbutton
+	ldi r16, $80   ; Enabling pull-up resistor for pushbutton
 	out PORTD, r16
 
-	ldi r16, $B3  ; PORTB as output MISO and both inputs of analog comparator as Input
+	ldi r16, $B3  ; Configure PORTB SPI pins (for LCD module connections)
+				  ; and both inputs of the analog comparator
 	out DDRB, r16
 
-//------------Stack Initialization------------//
+	;===============STACK INITIALIZATION===============;
 	ldi r16, LOW(RAMEND)	; load low byte of stack pointer
 	out SPL, r16
 	ldi r16, HIGH(RAMEND)	; load high byte of stack pointer
 	out SPH, r16
 
-//---------Initialize Variable Values---------//
-	ldi r16, $00      ; intitialzing counter flag (not counting)
-	sts counting, r16
-	sts meas_complete, r16 ; intializing meas_complete flag (measurement not complete)
+	;===============VARIABLE INITIALIZATION===============;
+	ldi r16, $00
+	sts counting, r16	   ; intitialzing counter flag (0 == not counting)
+	sts meas_complete, r16 ; intializing meas_complete flag (0 == measurement not complete)
+
+	;===============INITIALIZE LCD DOG MODULE===============;
 	call init_lcd_dog
 
-//------Configure Input Capture Interrupt------//
-//----------Configure Timer/Counter1----------//
-	ldi r16, $C4	  ; set to trigger at a rising edge & clkio/256
+	;===============CONFIGURE INPUTER CAPTURE INTERRUPT & TIMER/COUNTER1 ===============;
+	; Enable Input Capture noise canceler | Input capture triggers at rising edge |
+	; Counter clock = clkio/256 (from prescaler)
+	ldi r16, (1 << ICNC1) | (1 << ICES1) | (1 << CS12)
 	out TCCR1B, r16
-	ldi r16, 1 << TICIE1; enabling input capture interrupt
+	ldi r16, 1 << TICIE1 ; enable input capture interrupt for Timer/Counter 1
 	out TIMSK, r16
 
-	sei		; enable gloabl interrupt
+	sei ; enable gloabl interrupt
 
 main_loop:
-//-----------Polling Pushbutton Input-----------//
-	sbi PORTC, 0 ; deselect MAX5402
+	sbi PORTC, 0 ; deselect MAX5402 digital pot
 
-	ldi r16, 0 << TICIE1 ; enabling input capture interrupt
+	ldi r16, 0 << TICIE1 ; Disable input capture interrupt (while system is autocalibrating)
 	out TIMSK, r16
 
-	in r16, PIND ; calibrate voltage at Vce if push button at PD7 is pressed
-	sbrs r16, 7
-	call auto_calibrate
+	;===============AUTOCALIBRATION===============;
+	; in r16, PIND ; calibrate voltage at Vce if push button at PD7 is pressed
+	; sbrs r16, 7
+	; call auto_calibrate
 
-	ldi r16, 1 << TICIE1 ; enabling input capture interrupt
+	ldi r16, 1 << TICIE1 ; Re-enable input capture interrupt
 	out TIMSK, r16
 
-	cbi portB, 4 ; deselect DOG LCD
+	cbi portB, 4 ; deselect DOG LCD module
 
-//----------Polling meas_complete----------//
+	;===============CHECK IF PERIOD MEASUREMENT IS COMPLETE===============;
 	lds r16, meas_complete
-	sbrs r16, 0 	; skip if meas_complete flag is set
-
+	sbrs r16, 0 	; continue with calculation if meas_complete flag is set
 	rjmp main_loop
 
-//-----Dividend & Divisor Configuration-----//
-	ldi r18, LOW(2343750)
-	ldi r19, BYTE2(2343750)
-	ldi r20, BYTE3(2343750)
-	ldi r21, BYTE4(2343750)
+	;===============CONVERT PERIOD TO ===============;
+	;r21:r20:r19:r18 = Dividend = 0x23C346
+	ldi r18, LOW(CPM_CONSTANT)
+	ldi r19, BYTE2(CPM_CONSTANT)
+	ldi r20, BYTE3(CPM_CONSTANT)
+	ldi r21, BYTE4(CPM_CONSTANT)
 
+	;r25:r24:r23:r22 = Divisor = count captured (period of PPG)
 	lds r22, LOW(count_low)
 	lds r23, LOW(count_high)
 	ldi r24, $00
@@ -116,14 +124,15 @@ main_loop:
 
 	call div32u ; devide 2343750/count
 
-//--------Convert Division Result to BCD--------//
-
+	;===============CONVERT QUOTIENT TO BCD===============;
 	mov r16, r18
 	mov r17, r19
 
-	call bin2BCD16  ; convert current timer count to BCD
+	call bin2BCD16
 
-//-------------Convert BCD to ASCII-------------//
+	; TODO: Modify hex2ASCII to convert from BCD to ASCII instead
+	; So technically decimal to ascii
+	;===============CONVERT BCD TO ASCII===============;
 	mov r25, r14
 	lsr r14
 	lsr r14
@@ -176,39 +185,35 @@ main_loop:
 	call hex2ASCII
 	mov r13, r16
 
-//*---------Load & Update LCD Module---------//
-	call load_msg	; load ASCII into Buffer
-	call update_lcd_dog ; update the display
-						; with whats in the
-						; buffer
+	;===============UPDATE LCD MODULE===============;
+	call load_msg		; load ASCII characters into buffer
+	call update_lcd_dog ; update the display with buffer contents
 
-//------------Clear meas_complete------------//
-	ldi r16, $00
+	;===============READY FLAGS AND INPUT CAPTURE FOR NEXT MEASUREMENT===============;
+	ldi r16, $00		   ; reset measurement complete flag
 	sts meas_complete, r16
-
-//------Enable Input Caputer Interrupt------//
-	ldi r16, 1 << TICIE1; enabling input capture interrupt
+	ldi r16, 1 << TICIE1   ; re-enable input capture interrupt
 	out TIMSK, r16
 
 	rjmp main_loop
 
 ;==============================================================================
-//* "count_ISR"
-//*
-//* Description: Interrupt service routine, triggered when
-//* rising edge is going into ICP1 pin
-//*
-//*
-//* Author:		  Wilmer Suarez
-//* Version:      1.0
-//* Last updated: 11/29/2016
-//* Target:       ATmega16
-//* Number of words:
-//* Number of cycles:
-//* Low registers modified:
-//* High registers modified: r16
+; Subroutine name: "count_ISR"
+;
+; Description: Interrupt service routine, triggered when
+; rising edge is going into Timer/Counter1's Input Capture pin (ICPT1)
+;
+; Author:	    Wilmer Suarez
+; Version:      1.0
+; Last updated: 11/29/2016
+; Target:       ATmega16
+; Number of words:
+; Number of cycles:
+; Low registers modified:
+; High registers modified: r16
 ;==============================================================================
 count_ISR:
+	; Reserve used register values in stack
 	push r16
 	in r16, SREG
 	push r16
@@ -218,157 +223,71 @@ count_ISR:
 	rjmp counter_not_running
 	rjmp counter_running
 
-//--------------Counting = 0--------------//
+	;===============COUNTING == 0===============;
 counter_not_running:
-// stopping counter
-	ldi r16, $C0
-	out TCCR1B, r16
-
-// clearing counter value
+	;===============CLEAR OLD COUNTER VALUE===============;
 	clr r16
 	out TCNT1H, r16
 	out TCNT1L, r16
 
-// starting counter
-	ldi r16, $C4
+	;===============START COUNTER===============;
+	ldi r16, (1 << ICNC1) | (1 << ICES1) | (1 << CS12)
 	out TCCR1B, r16
-
-// counting flag set
-	ldi r16, $01
+	ldi r16, $01		; Set counting flag
 	sts counting, r16
 
-	pop r16
-	out SREG, r16
-	pop r16
+	rjmp count_ISR_done
 
-	reti ; return from ISR
-
-//--------------Counting = 1--------------//
+	;===============COUNTING == 1===============;
 counter_running:
-// stopping counter
-	ldi r16, $C0
+	;===============STOP COUNTER===============;
+	ldi r16, (1 << ICNC1) | (1 << ICES1)
 	out TCCR1B, r16
+	ldi r16, $00		; clear counting flag
+	sts counting, r16
 
-// reading counter value
-	in r16, ICR1L
+	;===============READ COUTNER VALUE===============;
+	in r16, ICR1L		; Low byte must be read first
 	sts count_low, r16
 	in r16, ICR1H
 	sts count_high, r16
 
-// counting flag cleared
-	ldi r16, $00
-	sts counting, r16
-
-// measurement complete variable is set
-	ldi r16, $01
+	ldi r16, $01			; set measurement complete flag
 	sts meas_complete, r16
 
-// disable Input Capture interrupt
+	;===============DISABLE INPUT CAPTURE INTERRUPT===============;
 	ldi r16, 0 << TICIE1; enabling input capture interrupt
 	out TIMSK, r16
 
+count_ISR_done:
+	; Restore old register values from stack
 	pop r16
 	out SREG, r16
 	pop r16
 
-	reti ; return from ISR
+	reti
 
-//--------------------------------------------
-//* "bin2BCD16" - 16-bit Binary to BCD conversion
-//*
-//* This subroutine converts a 16-bit number
-//* (fbinH:fbinL) to a 5-digit
-//* packed BCD number represented by 3 bytes
-//* (tBCD2:tBCD1:tBCD0).
-//* MSD of the 5-digit number is placed in the
-//* lowermost nibble of tBCD2.
-//*
-//* Number of words	:25
-//* Number of cycles	:751/768 (Min/Max)
-//* Low registers used	:3 (tBCD0,tBCD1,tBCD2)
-//* High registers used  :4
-//* (fbinL,fbinH,cnt16a,tmp16a)
-//* Pointers used	: Z
-//--------------------------------------------
-//***** Subroutine Register Variables
-.equ	AtBCD0	=13		;address of tBCD0
-.equ	AtBCD2	=15		;address of tBCD1
-
-.def	tBCD0	=r13		;BCD value digits 1
-;and 0
-.def	tBCD1	=r14		;BCD value digits
-; 3 and 2
-.def	tBCD2	=r15		;BCD value digit 4
-.def	fbinL	=r16		;binary value Low byte
-.def	fbinH	=r17		;binary value High byte
-.def	cnt16a	=r18		;loop counter
-.def	tmp16a	=r19		;temporary value
-
-//***** Code
-bin2BCD16:
-	ldi	cnt16a,16	;Init loop counter
-	clr	tBCD2		;clear result (3 bytes)
-	clr	tBCD1
-	clr	tBCD0
-	clr	ZH		;clear ZH
-	;(not needed for AT90Sxx0x)
-bBCDx_1:lsl	fbinL		;shift input value
-	rol	fbinH		;through all bytes
-	rol	tBCD0		;
-	rol	tBCD1
-	rol	tBCD2
-	dec	cnt16a		;decrement loop counter
-	brne	bBCDx_2		;if counter not zero
-	ret			;   return
-
-bBCDx_2:ldi	r30,AtBCD2+1	;Z points to result
-; MSB + 1
-bBCDx_3:
-	ld	tmp16a,-Z	;get (Z) with pre-decrement
-;---------------------------------------------
-;For AT90Sxx0x, substitute the above line with:
+;==============================================================================
+; Subroutine name: "hex2ASCII"
 ;
-;	dec	ZL
-;	ld	tmp16a,Z
+; Description:
+; Convert DIP switch value to ASCII to
+; place in the buffer
 ;
-;---------------------------------------------
-	subi	tmp16a,-$03	;add 0x03
-	sbrc	tmp16a,3	;if bit 3 not clear
-	st	Z,tmp16a	;	store back
-	ld	tmp16a,Z	;get (Z)
-	subi	tmp16a,-$30	;add 0x30
-	sbrc	tmp16a,7	;if bit 7 not clear
-	st	Z,tmp16a	;	store back
-	cpi	ZL,AtBCD0	;done all three?
-	brne	bBCDx_3		;loop again if not
-	rjmp	bBCDx_1
-
-//-------------------------------------
-//* Name - "hex2ASCII"
-//*
-//* Description:
-//* Convert DIP switch value to ASCII to
-//* place in the buffer
-//*
-//* Author: Wilmer Suarez
-//* Version: 1.0
-//* Last updated: 11/06/2016
-//* Target: ATmega16 @ 1 MHz
-//* Number of words:
-//* Number of cycles: 20
-//* Low registers used:
-//* High registers used: r16,r18
-//*
-//* Parameters:
-//* returns r16 loaded with ASCII
-//* form of DIP Switch input
-//*
-//* Notes:
-//-------------------------------------
+; Author: Wilmer Suarez
+; Version: 1.0
+; Last updated: 11/06/2016
+; Target: ATmega16 @ 1 MHz
+; Number of words:
+; Number of cycles:
+; Low registers used:
+; High registers used:
+;
+; Notes:
+;==============================================================================
 hex2ASCII:
 	cpi r16, 10 ; compare r16 with 10
-	brlo zero_9 ; branch to 0_to_9
-				; if r16 = $00-$09
+	brlo zero_9 ; branch to 0_to_9 if r16 < 10 (0 - 9)
 	subi r16, 10 ; subtract 10 from r16
 	subi r16, -$41 ; add $41 to r16
 	ret
@@ -377,28 +296,22 @@ hex2ASCII:
 	subi r16, -$30 ; add $30 to r16
 	ret
 
-//*******************************************
-//* "Subroutine_name" - load_msg
-//*
-//* Description:
-//* Loads a predefined string msg into a
-//* specified diplay buffer.
-//*
-//* Author: Wilmer Suarez
-//* Version:
-//* Last updated: 11/09/2016
-//* Target: ATmega16 @ 1 MHz
-//* Number of words:
-//* Number of cycles: 10
-//* Low registers used:
-//* High registers used: r16
-//*
-//* Parameters:
-//*  assumes: Z = offset of message to be loaded.
-//*  returns: buffer loaded with message
-//*  calls:  none
-//*  called by: main program and diagnostics
-//*****************************************
+;==============================================================================
+; Subroutine name: "load_msg"
+;
+; Description: Loads data into the diplay buffer.
+;
+; Author: Wilmer Suarez
+; Version: 1.0
+; Last updated: 11/09/2016
+; Target: ATmega16 @ 1 MHz
+; Number of words:
+; Number of cycles:
+; Low registers used:
+; High registers used: r16
+;
+; Notes:
+;==============================================================================
 load_msg:
 	sts dsp_buff + 0, r23 ; 1st digit
 	sts dsp_buff + 1, r24 ; 2nd digit
@@ -413,49 +326,50 @@ load_msg:
 
 	ret
 
-//*------------------------------------
-//* Subroutine Name: "auto_calibrate"
-//*
-//* Author: Wilmer Suarez
-//*
-//* Version: 1.0
-//*
-//* Last Updated:
-//* 12/06/2016
-//*
-//* Target: ATmega16A
-//*
-//* DESCRIPTION:
-//* This subroutine compares the output of a MAX5402 Digital Pot and
-//* the output of a voltage divider.
-//*------------------------------------
+;==============================================================================
+; Subroutine Name: "auto_calibrate"
+;
+; Description: This subroutine compares the output of a MAX5402 Digital
+; Potentiometer and the output of a voltage divider.
+;
+; Author: Wilmer Suarez
+; Version: 1.0
+; Last Updated: 12/06/2016
+; Target: ATmega16A
+; Number of words:
+; Number of cycles:
+; Low registers used:
+; High registers used:
+;
+; Notes:
+;==============================================================================
 auto_calibrate:
-	sbi portB, 4 ; set /SS of DOG LCD = 1 (Deselected)
-	cbi PORTC, 1 ; turn OFF LED
+	sbi PORTB, 4	; deselect LCD module
+	cbi PORTC, 1	; turn OFF LED
 
-//---Configuring SPI Control Register---//
-    ldi r16, $50  ; Enable SPI
-	out SPCR,r16  ; Data Order = MSB first
-	ldi r16, (1 << SPI2X) ; Set as Master
-	out SPSR, r16 ; SCK = fosc/2
+	;===============CONFIGURE SPI CONTROL REGISTER===============;
+	; Enable SPI | Data Order: MSB first | Set MCU as Master
+    ldi r16, (1 << SPE) | (1 << MSTR)
+	out SPCR,r16
+	ldi r16, (1 << SPI2X)	; SCK = fosc/2
+	out SPSR, r16
 
-//-------Clear SPI Interrupt Flag-------//
+	;===============CLEAR SPI INTERRUPT FLAG===============;
     in r16, SPSR
 	in r16, SPDR
 
-//--------Configure Analog Comparator--------//
+	;===============CONFIGURE ANALOG COMPARATOR===============;
 	ldi r16, $00
 	out ACSR, r16
-	ldi r16, $00 ; Initial Value for MAX5402 wiper position
+	ldi r16, $00	; Initial Value for MAX5402 wiper position
 
-//------------Send Data to Slave------------//
+	;===============SEND DATA TO DIGITAL POT (CURRENT SLAVE)===============;
 send_data:
-	cbi PORTC, 0  ; select MAX5402
-	out SPDR, r16 ; send data to the SPI Data Register
+	cbi PORTC, 0	; select MAX5402
+	out SPDR, r16	; send wiper data to the SPI Data Register
 
-//------Wait for Data to be Transfered------//
 wait:
-	sbis SPSR, SPIF
+	sbis SPSR, SPIF	; Wait for data transfer to complete
 	rjmp wait
 
 	sbi PORTC, 0   ; deselect MAX5402
